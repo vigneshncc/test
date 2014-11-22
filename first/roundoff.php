4df5f8b6fe8d8aac3071ba0abe5552af866108f6
asmatrichy <asmaitsoft@gmail.com>
2014-11-22 21:28:53 +0530

    Initial Commit

---
+++ first/roundoff.php
@@ -0,0 +1,11 @@
+<?php
+echo $input=25652252633361;
+echo "<br>";
+echo substr($input, -1); 
+echo "<br>";
+
+echo "<br>";
+echo round($input, -1);
+echo "<br>";
+echo $number = ceil($input / 10) * 10;
+?>
